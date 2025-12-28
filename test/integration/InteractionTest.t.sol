b30c029223ff07d22ebabda18efddb97dd4f982f
cj <Legolas-cj@outlook.com>
2025-12-28 10:59:02 +0800

    Fix integration test

--- test/integration/InteractionTest.t.sol
+++ test/integration/InteractionTest.t.sol
@@ -10,7 +10,7 @@ import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 contract InteractionTest is Test {
     FundMe fundMe;
 
-    address USER = makeAddr("user");
+    address public constant USER = address(1);
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
     uint256 constant GAS_PRICE = 1;
@@ -19,15 +19,29 @@ contract InteractionTest is Test {
         DeployFundMe deploy = new DeployFundMe();
         fundMe = deploy.run();
         console.log("chainid:", block.chainid);
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testUserCanFunderInteractions() public {
-        FundFundMe fundFundMe = new FundFundMe();
-        fundFundMe.fundFundMe(address(fundMe));
+        uint256 preUserBalance = address(USER).balance;
+        uint256 preOwnerBalance = address(fundMe.getOwner()).balance;
+        uint256 originalFundMeBalance = address(fundMe).balance;
+
+        // Using vm.prank to simulate funding from the USER address
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
 
         WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
         withdrawFundMe.withdrawFundMe(address(fundMe));
 
+        uint256 afterUserBalance = address(USER).balance;
+        uint256 afterOwnerBalance = address(fundMe.getOwner()).balance;
+
         assert(address(fundMe).balance == 0);
+        assertEq(afterUserBalance + SEND_VALUE, preUserBalance);
+        assertEq(
+            preOwnerBalance + SEND_VALUE + originalFundMeBalance,
+            afterOwnerBalance
+        );
     }
 }
